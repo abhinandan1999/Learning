5585e153901824743e81a9ce1aa29795ea84b344
abhinandanSigmoid <abhinandan.p@sigmoidanalytics.com>
2025-01-04 16:23:59 +0530

    Update Pandas utilities

---
+++ DataEngineering/Pandas Utility Functions/pandas_utils.py
@@ -0,0 +1,167 @@
+from typing import List, Dict
+
+import pandas as pd
+import numpy as np
+
+from functools import reduce
+
+####################################################################################
+                  ### DataFrame Utils ###
+####################################################################################
+
+def combine_dfs(dfs: List[pd.DataFrame], join_kw: Dict) -> pd.DataFrame:
+    """
+    Join the DataFrame on same key columns
+
+    Args:
+        dfs (List[pd.DataFrame]): List of DataFrames to be joined
+        join_kw (Dict): Key word argument to pd.Merge
+
+    Returns:
+        pd.DataFrame: Pandas DataFrame after merging the DataFrames
+    """
+
+    combine_dfs = reduce(
+        lambda df1, df2: df1.merge(df2, **join_kw),
+        dfs
+    )
+
+    return combine_dfs
+
+
+
+
+####################################################################################
+                  ### Attributes Summaries Utilities ###
+####################################################################################
+def _get_dtypes(df):
+    """
+    Helper function to get data types of features
+
+    Args:
+        df (pd.DataFrame): Input DataFrame with attributes
+
+    Returns:
+        pd.DataFrame: DataFrame with feature names and its data type
+    """
+    ft_dtype = df.dtypes
+    ft_dtype_df = pd.DataFrame({
+        'Attribute Name': ft_dtype.index,
+        'Data Type': ft_dtype.index.values
+    })
+
+    return ft_dtype_df
+
+def _get_rows_count(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Get total rows count and total unique rows count, unique values count and Null Values count
+
+    Args:
+        df (pd.DataFrame): Pandas Dataframe to perform count operations
+    Return:
+        pd.DataFrame: Total Rows count and unqiue Rows count, unique values count and Null Values count across all attributes.
+    """
+
+    # Total Rows and Total unique rows count
+    total_rows, total_unique_rows = df.shape[0], df.drop_duplicates().shape[0]
+
+    # Unique Value counts
+    counts_df = (
+        df.agg(["nunique", lambda x: x.isna().sum()])
+        .T.reset_index(names="Attribute Name")
+        .rename(columns={"nunique": "# of Unique Values", "<lambda>": "# of Null Values"})
+        .assign(
+            **{
+                "# of Rows": total_rows,
+                "# of Unique Rows": total_unique_rows,
+                "% Unique Values": lambda x: x["# of Unique Values"]
+                / x["# of Unique Rows"],
+                "% Null Values": lambda x: x["# of Null Values"] / x["# of Unique Rows"],
+            }
+        )
+    )
+
+    return counts_df
+
+
+def _get_top_n_values(df: pd.DataFrame, top_n: int) -> pd.DataFrame:
+    """
+    Gets Top n most frequent occuring values
+
+    Args:
+        df (pd.DataFrame): Pandas DataFrame with attributes to calculate Top n
+
+    Returns:
+        pd.DataFrame: DataFrame with attributes and Top n Values
+    """
+
+    def _get_top_n_value_count(ser: pd.Series, col_name: str, top_n: int) -> List[Dict]:
+        top_n_values = (
+            ser.value_counts()
+            .reset_index()
+            .assign(
+                **{
+                    f"{col_name}": lambda x: np.where(
+                        x.index >= top_n, "Other_Values", x[col_name]
+                    )
+                }
+            )
+            .groupby([col_name], as_index=False)
+            .agg(**{"# of Occurence": ("count", "sum")})
+            .sort_values("# of Occurence", ascending=False)
+            .apply(lambda x: {x[col_name]: x["# of Occurence"]}, axis=1)
+            .values
+        )
+
+        return top_n_values
+
+    value_counts_dict = {}
+    for col in df.columns:
+        top_n_values = _get_top_n_value_count(df[col], col, top_n)
+        value_counts_dict.update({col: [top_n_values]})
+
+    value_counts_df = (
+        pd.DataFrame.from_dict(value_counts_dict, orient="index")
+        .reset_index(names="Attribute Name")
+        .rename(columns={0: f"Top {top_n} values"})
+    )
+
+    return value_counts_df
+
+
+def get_df_summary(df:pd.DataFrame, top_n: int=5) -> pd.DataFrame:
+    """
+    Generate the summaries of the attributes in a pandas DataFrame.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame with attributes to be summarized.
+        top_n (int): Number of Top n occuring values in a attribute of DataFrame
+
+    Returns:
+        tuple:
+            - pd.DataFrame: A summary DataFrame containing feature name, unique value count, uniqueness ratio, percentage of null values, null bucket, and data type.
+            - pd.DataFrame: A summary of the null value buckets.
+    """
+
+    # Get Data Types of Columns
+    dataType_df = _get_dtypes(df)
+
+    # Get Count total of Columns
+    row_total_df = _get_rows_count(df)
+
+    # Get Top n Value counts
+    top_n_values_df = _get_top_n_values(df, top_n=top_n)
+
+    # Combine these datasets to create a single dataFrame
+    dfs = [
+        dataType_df, row_total_df, top_n_values_df
+    ]
+    join_kw = {
+        'on': ['Attribute Name'],
+        'how': 'outer'
+    }
+    attribute_summary_df = combine_dfs(dfs, join_kw=join_kw)
+
+    return attribute_summary_df
+
+    
