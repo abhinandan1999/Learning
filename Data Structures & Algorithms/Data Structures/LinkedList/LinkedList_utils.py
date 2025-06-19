239131b8347a4ca1068b4ec0760d13430aa778ba
abhinandanSigmoid <abhinandan.p@sigmoidanalytics.com>
2025-06-19 23:34:44 +0530

    Update Linked List Learning

--- Data Structures & Algorithms/Data Structures/LinkedList/LinkedList_utils.py
+++ Data Structures & Algorithms/Data Structures/LinkedList/LinkedList_utils.py
@@ -1,9 +1,30 @@
+# TODO
+# 1. Insert
+#   - Start
+#   - End
+#   - Before a Node
+#   - After a Node
+
+# 2. Remove
+#   - Star
+#   - End
+#   - Before a Node
+#   - After a Node
+
+# 3. Retrieve
+#   - From a Index
+
+
 # Create a Node Class
 class Node:
     def __init__(self, value):
-        """Initialize the Node"""
-        self.data = value  # TC: O(1), SC: O(1)
-        self.next = None  # TC: O(1), SC: O(1)
+        """
+        Initialize the Node
+        TC: (1)
+        SC: O(1)
+        """
+        self.data = value
+        self.next = None
 
     def __repr__(self):
         """
@@ -86,44 +107,143 @@ class LinkedList:
     def __len__(self):
         return self.len
 
-    def insert(self, data, index):
-        """Insert the Element at a specific Index"""
+    def insertBeg(self, data):
+        # TC: O(1)
+        # SC: O(1)
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+        # Increment the Length
+        self.len += 1
+
+        return 
+
+    def insertEnd(self, data):
+        # TC: O(n)
+        # SC: O(1)
+        new_node = Node(data)
+
+        # Check if self.head is None
+        if not self.head:
+            self.head = new_node
+            return 
         
-        # Check if the Index is greater than Length of Linked List + 1
-        if index > self.len:
-            raise Exception(f"Index {index} is greater than Linked List of length {self.len}")
+        # Travel till End of Node
+        for current_node in self:
+            pass
+
+        current_node.next = new_node
+
+        # Increment the Length
+        self.len += 1
+
+        return
+
+    def insertBefore(self, data, index):
+        # TC: O(n)
+        # SC: O(1)
+        # Check if the Index is within Range
+        if (index < 0) or (index > self.len - 1):
+            raise Exception(f"Index {index} is out of Range. Length of Linked List {self.len}")
         
-        else:
-            
-            # Create an instance of Node
-            node_to_insert = Node(data)  
+        # If the Index is 0 then it same as inserting at beginning
+        if index == 0:
+            self.insertBeg(data)
 
-            # Travel to the Index
-            node = head = self.head
-            curr_pos = 0
-            while curr_pos < index-1:
-                node = node.next
-                curr_pos += 1
+        new_node = Node(data)
 
-            # Set the Next Node
-            if index == 0:
-                self.head = node_to_insert
-                node_to_insert.next = node
-            else:
-                node_to_insert.next = node.next
-                node.next = node_to_insert
+        # Travel to the Index -1
+        current_node = self.head
+        for _ in range(0, index-1):
+            current_node = current_node.next
+        
+        # Adjust the Links
+        new_node.next = current_node.next
+        current_node.next = new_node
 
+        # Increment the Length by 1
+        self.len += 1
 
-            # Increment the Length of Linked List by 1
-            self.len += 1
+        return 
+
+    def inserAfter(self, data, index):
+        # TC: O(1)
+        # SC: O(1)
+        # Check if the Index is within Range
+        if (index < 0) or (index > self.len - 1):
+            raise Exception(f"Index {index} is out of Range. Length of Linked List {self.len}")
+        
+        new_node = Node(data)
+
+        # Travel till the Index
+        current_node = self.head
+        for _ in range(0, index):
+            current_node = current_node.next
+
+        new_node.next = current_node.next
+        current_node.next = new_node
+
+        # Increment the length by 1
+        self.len += 1
+
+        return 
                 
     def remove(self, data):
         """Deletes the specific Data if found"""
-        pass
+        # TC: O(N)
+        # SC: O(1)
+
+        # Check if LL is Null
+        if not self.head:
+            raise Exception("Linked List is empty")
+
+        # Check if the data is at head
+        if self.head.data == data:
+            self.head = self.head.next
+
+            # Adjust the Length
+            self.len -= 1
+
+            return 
+        
+        # Travel till the data
+        previous_node = self.head
+        for current_node in self:
+            if current_node.data == data:
+                # Adjust the links
+                previous_node.next = current_node.next
+                
+                # Adjust the Length
+                self.len -= 1
+                return
+            previous_node = current_node 
+
+        raise Exception(f"{data} not found in Linked List")
+        
 
     def remove_index(self, index):
         """Removes the specific Index"""
-        pass
+
+        # Check if the Index is within Range
+        if (index < 0) or (index > self.len - 1):
+            raise Exception(f"Index {index} is out of Range. Length of Linked List {self.len}")
+        
+        # Check if first element has to be removed
+        if index == 0:
+            self.head = self.head.next
+            self.len -= 1
+            return
+        
+        # Travel Till Index - 1
+        current_node = self.head
+        for _ in range(0, index-1):
+            current_node = current_node.next
+
+        # Adjust the Links
+        current_node.next = current_node.next.next
+        self.len -= 1
+        return 
 
 
 def main():
@@ -132,22 +252,29 @@ def main():
     linked_list = LinkedList(["1", "2", "3", "4"])
     print(f"Intial Linked List of length {len(linked_list)} {linked_list}\n")
 
-    print(f"Inserting {10} at Index {0}")
-    linked_list.insert("10", 0)
+    print(f"Inserting {10} at Beginning")
+    linked_list.insertBeg("10")
     print(f"Linked List of length {len(linked_list)} {linked_list}\n")
     
-    print(f"Inserting {20} at Index {1}")
-    linked_list.insert("20", 1)
+    print(f"Inserting {20} at End")
+    linked_list.insertEnd("20")
     print(f"Linked List of length {len(linked_list)} {linked_list}\n")
 
-    print(f"Inserting {40} at Index {4}")
-    linked_list.insert("40", 4)
+    print(f"Inserting {40} before index 4")
+    linked_list.insertBefore("40", 4)
     print(f"Linked List of length {len(linked_list)} {linked_list}\n")
 
-    print(f"Inserting {100} at Index {len(linked_list)}")
-    linked_list.insert("100", len(linked_list))
+    print(f"Inserting {100} at after Index 2")
+    linked_list.inserAfter("100", 6)
+    print(f"Linked List of length {len(linked_list)} {linked_list}\n")
+    
+    print(f"Removing 40 from the Linked List")
+    linked_list.remove("40")
+    print(f"Linked List of length {len(linked_list)} {linked_list}\n")
+
+    print(f"Removing from index 3")
+    linked_list.remove_index(6)
     print(f"Linked List of length {len(linked_list)} {linked_list}")
-    
     # for ele in linked_list:
     #     print(ele)
 
