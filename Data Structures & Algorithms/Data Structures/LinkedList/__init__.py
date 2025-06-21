d4bc5067b4d25cc9fba007f71b41643eeef37869
abhinandanSigmoid <abhinandan.p@sigmoidanalytics.com>
2025-06-22 01:45:19 +0530

    Update Llist Learning

--- Data Structures & Algorithms/Data Structures/LinkedList/__init__.py
+++ Data Structures & Algorithms/Data Structures/LinkedList/__init__.py
@@ -1,4 +1,3 @@
 from .LinkedList_utils import LinkedList
-# from .linkedListAlgos import recursive_travel
 
-__all__ = ["LinkedList", "recursive_travel"]
+__all__ = ["LinkedList"]
--- Data Structures & Algorithms/Data Structures/LinkedList/linkedListAlgos/__init__.py
+++
@@ -1,3 +0,0 @@
-# from .travel_linked_list import recursive_travel
-
-__all__ = ["recursive_travel"]
