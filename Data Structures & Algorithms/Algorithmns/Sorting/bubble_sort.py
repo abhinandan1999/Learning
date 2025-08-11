550065637bfadfd5228a80850d057fda51eb8e30
abhinandanSigmoid <abhinandan.p@sigmoidanalytics.com>
2025-08-11 20:37:38 +0530

    Update code

---
+++ Data Structures & Algorithms/Algorithmns/Sorting/bubble_sort.py
@@ -0,0 +1,17 @@
+# python -m Algorithmns.Sorting.bubble_sort
+from typing import List
+
+def bubble_sort(list: List) -> List:
+
+    return list
+
+def main():
+
+    list_ = [1, 2, 3, 4]
+    print(f"Original Array: {list_}")
+
+    sorted_list_ = bubble_sort(list=list_)
+    print(f"Sorted Array: {sorted_list_}")
+
+if __name__ == "__main__":
+    main()
---
+++ Data Structures & Algorithms/Algorithmns/Sorting/sorting.md
@@ -0,0 +1,13 @@
+# ✅ Sorting Algorithms
+
+- [ ] Bubble Sort
+- [ ] Insertion Sort
+- [ ] Selection Sort
+- [ ] Merge Sort
+- [ ] Quick Sort
+- [ ] Heap Sort
+- [ ] Counting Sort
+- [ ] Radix Sort
+- [ ] Bucket Sort
+- [ ] Shell Sort
+- [ ] Tim Sort
