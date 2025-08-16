4f44289465bc8f145b16f5ace785c9bcf3c377f3
abhinandanSigmoid <abhinandan.p@sigmoidanalytics.com>
2025-08-16 23:38:31 +0530

    Add Bubble sort

--- Data Structures & Algorithms/Algorithmns/Sorting/bubble_sort.py
+++ Data Structures & Algorithms/Algorithmns/Sorting/bubble_sort.py
@@ -1,21 +1,44 @@
 # python -m Algorithmns.Sorting.bubble_sort
 from typing import List
 
-def bubble_sort(list: List) -> List:
+def bubble_sort(list_: List) -> List:
     """Sort the list with bubble sort algorithm"""
     # TC: O()
     # SC: O()
 
-    sorted_list = list
-    
-    return sorted_list
+    # Get Length of the list
+    n = len(list_)
+
+    # Iterate over each element
+    for i in range(0, n-1):
+        
+        # Flag to check if Swap was made, if no then the array is already sorted
+        is_swap = False
+
+        # Since after every iteration each element will be moved to end.
+        # Subsequent iteration need not iterate till last element (n - i - 1)
+        for j in range(0, n-i-1):
+            # Compare the adjacent element
+            if list_[j] > list_[j+1]:
+                # Swap them
+                list_[j], list_[j+1] = list_[j+1], list_[j]
+                is_swap = True
+
+        # Check if no swap was made
+        if not is_swap:
+            break
+
+        # After end of each iteration the greatest element will be pushed to end
+        print(f"List after Iteration {i+1}: {list_}")
+
+    return list_
 
 def main():
 
-    list_ = [1, 2, 3, 4]
+    list_ = [4, 3, 2, 1]
     print(f"Original Array: {list_}")
 
-    sorted_list_ = bubble_sort(list=list_)
+    sorted_list_ = bubble_sort(list_=list_)
     print(f"Sorted Array: {sorted_list_}")
 
 if __name__ == "__main__":
