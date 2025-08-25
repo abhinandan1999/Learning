074352601a91fe85d8f156de2f426cb21cefc93e
abhinandanSigmoid <abhinandan.p@sigmoidanalytics.com>
2025-08-25 23:06:15 +0530

    Add Selection Sort

---
+++ Data Structures & Algorithms/Algorithmns/Sorting/selection_sort.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def selection_sort(list_: List):
+
+    return list_
+
+def main():
+    
+    list_ = [4, 3, 2, 1]
+    print(f"Original List: {list_}")
+
+    sorted_list = selection_sort(list_=list_)
+    print(f"Sorted List: {sorted_list}")
+
+if __name__ == "__main__":
+    main()
