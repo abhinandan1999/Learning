b1a5fcb5bcf683ba4807b48bbe493424b7ac9b2b
abhinandanSigmoid <abhinandan.p@sigmoidanalytics.com>
2025-08-25 23:03:04 +0530

    Implement Insertion Sort

--- Data Structures & Algorithms/Algorithmns/Sorting/insertion_sort.py
+++ Data Structures & Algorithms/Algorithmns/Sorting/insertion_sort.py
@@ -1,6 +1,19 @@
 # python -m Algorithmns.Sorting.insertion_sort
 from typing import List
 
+
+def binary_search(arr: List, target: int, start: int, end: int) -> int:
+    """
+    Perform binary search to find the correct position for the target in the sorted subarray.
+    """
+    while start < end:
+        mid = (start + end) // 2
+        if arr[mid] < target:
+            start = mid + 1
+        else:
+            end = mid
+    return start
+
 def insertion_sort(list_: List) -> List:
     """
     Sort the list using insertion sort algorithm.
@@ -20,11 +33,28 @@ def insertion_sort(list_: List) -> List:
         
         # Step 1: Assume List is sorted till index i
         # Step 2: Find appropriate place for element at index (i + 1)
-        # Step 3: Keep swapping until appropriate index is found
-        for j in range(i, 0, -1):
-            # Check if list_[i+1] is smaller than the last element of sorted array i.e. list_[i]
-            if list_[j] < list_[j-1]:
-                list_[j-1], list_[j] = list_[j], list_[j-1]
+        
+
+        # Method 1:  Keep swapping until appropriate index is found
+        # for j in range(i, 0, -1):
+        #     # Check if list_[i+1] is smaller than the last element of sorted array i.e. list_[i]
+        #     if list_[j] < list_[j-1]:
+        #         list_[j-1], list_[j] = list_[j], list_[j-1]
+
+        # Method 2: Keep Shifting element until appropriate index is found
+        # j = i
+        # ele_to_reindex = list_[j]
+        # while (j > 0) and (ele_to_reindex < list_[j-1]):
+        #     list_[j] = list_[j-1]
+        #     j -= 1
+        
+        # list_[j] = ele_to_reindex
+
+        # Method 3: Find the index to insert the element using binary search (As Left Subarray is sorted)
+        ele_to_reindex = list_[i]
+        k = binary_search(list_, ele_to_reindex, 0, i)
+        list_[k+1:i+1] = list_[k:i]
+        list_[k] = ele_to_reindex
 
         print(f"List after Iteration {i} is: {list_}")
 
@@ -33,7 +63,7 @@ def insertion_sort(list_: List) -> List:
 
 def main():
 
-    list_ = [3, 2, 1, 4, 0, -1]
+    list_ = [3, 4,22, 13, 0, -1, 44]
     print(f"Original Array: {list_}")
 
     sorted_list_ = insertion_sort(list_=list_)
