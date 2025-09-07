d697b4a1ba4630bcdf86beda7f85410e84f34a9f
abhinandanSigmoid <abhinandan.p@sigmoidanalytics.com>
2025-09-07 20:20:35 +0530

    Add Merge Sort

---
+++ Data Structures & Algorithms/Algorithmns/Sorting/04_Merge_sort.py
@@ -0,0 +1,20 @@
+# python -m Algorithmns.Sorting.04_Merge_sort
+from typing import List
+
+def merge_sort(list_: List) -> List:
+    """
+    Sort the List using merge sort
+    """
+    # TC: O(n*log(n))
+    # SC: 
+    # Stability:
+
+    return list_
+
+if __name__ == "__main__":
+
+    list_ = [4, 3, 2, 1]
+    print(f"Intial List: {list_}")
+
+    sorted_list = merge_sort(list_)
+    print(f"Final sorted List: {sorted_list}")
