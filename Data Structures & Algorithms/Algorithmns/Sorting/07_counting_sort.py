1a8fd480ba18cbf6215d00a69703d224a62e0511
abhinandanSigmoid <abhinandan.p@sigmoidanalytics.com>
2025-09-23 22:47:17 +0530

    Add Counting sort

---
+++ Data Structures & Algorithms/Algorithmns/Sorting/07_counting_sort.py
@@ -0,0 +1,19 @@
+# python -m 07_counting_sort
+from typing import List
+
+def counting_sort(list_: List) -> List:
+    """Implement a counting sort algorithm"""
+    # TC: 
+    # SC:
+    # Stability:
+
+    return list_
+
+
+if __name__ == "__main__":
+
+    list_ = [4, 3, 1, 2]
+    print(f"Original List before sorting: {list_}")
+
+    sorted_list = counting_sort(list_)
+    print(f"List after sorting: {list_}")
